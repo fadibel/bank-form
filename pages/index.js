75d4b29b564db079ca1e27b861368af00a5ce118
text <text@gtext.gdsg>
2024-08-14 00:03:05 +0100

    new index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import LoanForm from "./LoanForm";
-
+import { UserContext } from "./contexts/UserContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+
+  
   return (
+
+    <UserContext.Provider>
 
     <div className="flex justify-center text-black mt-64 ">
       <LoanForm/>
     </div>
+    </UserContext.Provider>
    
   );
 }
